add2c59e2dcbeb1757031035e476ddfb8a7c9fc6
Tom Offermann <toffermann@newrelic.com>
2016-10-01 09:27:34 -0700

    Log trace output in filename_tracer.
    No more printing to stdout.

--- examples/scripts/filename_trace.py
+++ examples/scripts/filename_trace.py
@@ -1,14 +1,23 @@
 import linecache
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+        format='%(message)s',
+        filename='logs/trace.log',
+        filemode='w',
+        level=logging.INFO
+)
 
 def filename_tracer(frame, event, arg):
     fname = frame.f_code.co_filename
     co_name = frame.f_code.co_name
-    lineno = frame.f_lineno
-    line = linecache.getline(fname, lineno)
+    num = frame.f_lineno
+    line = linecache.getline(fname, num)
 
-    if event == "call":
-        print("--- %s: %s" % (fname, co_name))
-    else:
-        print("%s(%d): %s" % (fname, lineno, line))
+    if event == 'call':
+        logger.info("--- %s: %s", fname, co_name)
+    elif event == 'line':
+        logger.info("%s(%d): %s", fname, num, line)
 
     return filename_tracer
